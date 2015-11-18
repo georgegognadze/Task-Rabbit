ba9fcd56a2893e5829a2bb1eb15783a34352cacc
dreamtocode <gognadzegiorgi@gmail.com>
2015-11-18 11:25:43 +0400

    create views

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html'
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
