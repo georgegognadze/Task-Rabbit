3d80a1bff5e35e2106b722ef8918d0fa936f22fd
dreamtocode <gognadzegiorgi@gmail.com>
2015-11-27 02:28:01 +0400

    connect to database

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ var app = angular
     'ngRoute',    
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://clone-task-ninja.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
--- scripts/task.js
+++ scripts/task.js
@@ -1,7 +1,10 @@
 'use strict';
 
-app.controller('TaskController', function($scope) {
+app.controller('TaskController', function($scope, FURL, $firebase) {
+	var ref = new Firebase(FURL);
+	var fbTasks = new $firebase(ref.child('tasks')).$asArray();
+
 	$scope.postTask = function(task) {
-		console.log(task);
+		fbTasks.$add(task);
 	}
 });
