d929982d4b944d402830aec2393e60a2eab75b2d
dreamtocode <gognadzegiorgi@gmail.com>
2015-11-29 00:05:00 +0400

    work with controller

--- scripts/task.js
+++ scripts/task.js
@@ -1,10 +1,14 @@
 'use strict';
 
-app.controller('TaskController', function($scope, FURL, $firebase) {
+app.controller('TaskController', function($scope, FURL, $firebase, $location) {
+	
 	var ref = new Firebase(FURL);
-	var fbTasks = new $firebase(ref.child('tasks')).$asArray();
+	var fbTasks = $firebase(ref.child('tasks')).$asArray();
+
+	$scope.tasks = fbTasks;
 
 	$scope.postTask = function(task) {
 		fbTasks.$add(task);
+		$location.path('/browse');
 	}
 });
