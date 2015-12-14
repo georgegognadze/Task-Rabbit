c9b8fef8cd4e4f6267a5f5e6ab1ace5345e84221
dreamtocode <gognadzegiorgi@gmail.com>
2015-12-15 01:24:20 +0400

    delete task.js add indent

--- scripts/Services/auth.js
+++ scripts/Services/auth.js
@@ -4,36 +4,36 @@ app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {
 
 	var ref = new Firebase(FURL);
 	var auth = $firebaseAuth(ref);
-}
+
 var Auth = {
 
 	user: {},
 
 	login: function(user) {
 		return auth.$authWithPassword(
-			{email: user.email, password: user.password})
-	};
-},
+			{email: user.email, password: user.password}
+	);
+	},
 	register: function(user) {
 		return auth.$createUser(
 			{email: user.email, password: user.password})
-		.then(function()) {
+		.then(function() {
 			return Auth.login(user);
 	});
-},
+	},
 	logout: function() {
 		auth.$unauth();
 	},
 
 	changePassword: function(user) {
 		return auth.$changePassword({email: user.email, oldPassword: user.oldpass, newPassword: user.newpass});
-},
+	},
 	signedIn: function() {
 		return !!Auth.user.provider;
 	}
-};
+	};
 
-	auth.$onAuth(function(authData)) {
+	auth.$onAuth(function(authData) {
 		if(authData) {
 			angular.copy(authData, Auth.user);
 		} else {
