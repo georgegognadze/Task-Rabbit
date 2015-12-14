e0b6efbfc7a7ae6e5da638d880328efcfb8308b6
dreamtocode <gognadzegiorgi@gmail.com>
2015-12-14 22:03:03 +0400

    add missing lines of code to auth.js
    listen to users

--- scripts/Services/auth.js
+++ scripts/Services/auth.js
@@ -1,32 +1,39 @@
+'use strict';
+
+app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {
+
+	var ref = new Firebase(FURL);
+	var auth = $firebaseAuth(ref);
+}
 var Auth = {
 
 	user: {},
 
 	login: function(user) {
-		return Auth.$AuthWithPassword(
+		return auth.$authWithPassword(
 			{email: user.email, password: user.password})
 	};
 },
 	register: function(user) {
-		return Auth.$createUser(
+		return auth.$createUser(
 			{email: user.email, password: user.password})
 		.then(function()) {
 			return Auth.login(user);
 	});
 },
 	logout: function() {
-		Auth.$unauth();
+		auth.$unauth();
 	},
 
 	changePassword: function(user) {
-		return Auth.$changePassword({email: user.email, oldPassword: user.oldpass, newPassword: user.newpass});
+		return auth.$changePassword({email: user.email, oldPassword: user.oldpass, newPassword: user.newpass});
 },
 	signedIn: function() {
 		return !!Auth.user.provider;
 	}
 };
 
-	Auth.$onAuth(function(authData)) {
+	auth.$onAuth(function(authData)) {
 		if(authData) {
 			angular.copy(authData, Auth.user);
 		} else {
