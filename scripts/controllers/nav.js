a08929ccc5f707e8f629f63acb74061d5bd489ff
dreamtocode <gognadzegiorgi@gmail.com>
2015-12-27 23:55:23 +0400

    add gravatar

--- scripts/controllers/nav.js
+++ scripts/controllers/nav.js
@@ -1,7 +1,8 @@
 'use strict';
 
 app.controller('NavController', function($scope, $location, Auth) {
-
+	
+	$scope.currentUser = Auth.user;
 	$scope.signedIn = Auth.signedIn;
 
 	$scope.logout = function() {
