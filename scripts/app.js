2122672cbf25ac6e317df7e583ce6b60de639b7c
dreamtocode <gognadzegiorgi@gmail.com>
2015-12-29 23:35:27 +0400

    change nav & remove post, edit

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ var app = angular
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/browse.html'        
       })
       .when('/login', {
         templateUrl: 'views/login.html',   
@@ -21,14 +21,6 @@ var app = angular
       .when('/register', {
         templateUrl: 'views/register.html',
         controller: 'AuthController'
-      })
-      .when('/post', {
-        templateUrl: 'views/post.html',
-        controller: 'TaskController'
-      })
-      .when('/edit/:taskId', {
-        templateUrl: 'views/edit.html',
-        controller: 'TaskController'
       })
       .when('/browse', {
         templateUrl: 'views/browse.html',
