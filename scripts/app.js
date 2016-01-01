ed7af1ffdaff5453c62fa9d41a1da5c62cda7bbd
dreamtocode <gognadzegiorgi@gmail.com>
2016-01-01 23:51:35 +0400

    add controller

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,8 @@ var app = angular
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/browse.html'        
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController'        
       })
       .when('/login', {
         templateUrl: 'views/login.html',   
@@ -23,9 +24,9 @@ var app = angular
         templateUrl: 'views/register.html',
         controller: 'AuthController'
       })
-      .when('/browse', {
+      .when('/browse/:taskId', {
         templateUrl: 'views/browse.html',
-        controller: 'TaskController'
+        controller: 'BrowseController'
       })
       .otherwise({
         redirectTo: '/'
