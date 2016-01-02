773bcd6164c7cd7f5d65512907ea8f77e55256e1
dreamtocode <gognadzegiorgi@gmail.com>
2016-01-02 23:35:53 +0400

    add controller

---
+++ scripts/controllers/browse.js
@@ -0,0 +1,9 @@
+ 'use strict';
+
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
+
+	$scope.searchTask = '';		
+	$scope.tasks = Task.all;
+	$scope.signedIn = Auth.signedIn;
+	$scope.listMode = true;
+});
