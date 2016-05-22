24cacd1d15abf1709af9f24c0f254453b6a3d7f3
dreamtocode <gognadzegiorgi@gmail.com>
2016-05-22 21:21:54 +0400

    add parameters

--- scripts/task.js
+++ scripts/task.js
@@ -1,6 +1,6 @@
 'use strict'
 
-app.controller('TaskController', function($scope) {
+app.controller('TaskController', function($scope, FURL, $firebase) {
 
 	$scope.postTask = function(task) {
 		console.log(task);
