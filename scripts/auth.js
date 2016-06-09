28d0101331e67c6388b98978bd0e5ae8f856013d
dreamtocode <gognadzegiorgi@gmail.com>
2016-06-09 20:57:22 +0400

    add auth
    firebaseAuth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,8 @@
 'use strict';
 
-app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {});
+app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {
+
+	var ref = new Firebase(FURL);
+	var auth = $firebaseAuth(ref);
+
+});
