53ae7184a9a23ad9c7fdbffda002f57cf55bde85
dreamtocode <gognadzegiorgi@gmail.com>
2016-06-09 21:12:11 +0400

    add Auth login

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,4 +5,15 @@ app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {
 	var ref = new Firebase(FURL);
 	var auth = $firebaseAuth(ref);
 
+	var Auth = {
+
+		login: function(user) {
+			return auth.$authWithPassword(
+				{email: user.email, password: user.password}
+			);
+		}
+	};
+
+	return Auth;
+
 });
