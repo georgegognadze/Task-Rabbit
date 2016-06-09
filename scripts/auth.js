1331269407ac3fa3f87e1c8af0cb42efa6fac9ea
dreamtocode <gognadzegiorgi@gmail.com>
2016-06-09 21:21:22 +0400

    add Auth register

--- scripts/auth.js
+++ scripts/auth.js
@@ -11,6 +11,12 @@ app.factory('Auth', function(FURL, $firebaseAuth, $firebase) {
 			return auth.$authWithPassword(
 				{email: user.email, password: user.password}
 			);
+		},
+		register: function(user) {
+			return auth.$createUser({email: user.email, password: user.password})
+			.then(function() {
+				return Auth.login(user);
+			});
 		}
 	};
 
