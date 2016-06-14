2355b4ca6bd4152c0ae78302afd605f48cb4a088
George Gognadze <gognadzegiorgi@gmail.com>
2016-06-14 23:44:25 +0400

    add updated Auth.js

--- scripts/controllers/auth.js
+++ scripts/controllers/auth.js
@@ -1,5 +1,12 @@
 'user strict';
 
-app.controller('AuthController', function($scope, $location) {
-	// body...
+app.controller('AuthController', function($scope, $location, Auth) {
+	$scope.register = function(user) {
+		Auth.register(user).then(function() {
+			console.log("Register successfully!");
+			$location.path('/');
+		}, function(err) {
+			console.log("Error...");
+		}); 
+	}
 });
