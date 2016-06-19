cf846158c563f9bf03395245404faad2e7e329f1
George Gognadze <gognadzegiorgi@gmail.com>
2016-06-19 23:08:08 +0400

    add signedIn

--- scripts/controllers/nav.js
+++ scripts/controllers/nav.js
@@ -2,4 +2,12 @@
 
 app.controller('NavController', function($scope, $location, Auth) {
 
+	$scope.signedIn = Auth.signedIn;
+
+	$scope.logout = function() {
+		Auth.logout();
+		console.log("Logged out!");
+		$location.path('/');
+	};
+	
 });
