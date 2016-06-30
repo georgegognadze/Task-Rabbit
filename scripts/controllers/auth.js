395cfdd16c6c09e57c9faae14bd8cfce911f30ca
George Gognadze <gognadzegiorgi@gmail.com>
2016-07-01 00:04:19 +0400

    add more toaster notifications in auth.js

--- scripts/controllers/auth.js
+++ scripts/controllers/auth.js
@@ -15,9 +15,11 @@ app.controller('AuthController', function($scope, $location, Auth, toaster) {
 	$scope.login = function(user) {
 		Auth.login(user)
 		.then(function() {
+			toaster.pop('success', 'Logged in successfully');
 			// console.log("Logged in successfully!");
 			$location.path('/');
 		},  function(err) {
+			toaster.pop('error', 'Oops, something went wrong');
 			// console.log("Error...");
 		});
 	};
@@ -31,9 +33,10 @@ app.controller('AuthController', function($scope, $location, Auth, toaster) {
 			$scope.user.email = '';
 			$scope.user.oldPass = '';
 			$scope.user.newPass = '';
-
+			toaster.pop('success', 'Password changed successfully!');
 			// console.log("Password changed successfully!");
 		}, function(err) {
+			toaster.pop('error', 'Oops, something went wrong');
 			// console.log("Error...");
 		});
 	};
