b394d05510293d876e04fff45f28b726797c9b39
George Gognadze <gognadzegiorgi@gmail.com>
2016-07-04 23:18:04 +0400

    remove post and edit from app.js

--- scripts/app.js
+++ scripts/app.js
@@ -25,14 +25,6 @@ var app = angular
         templateUrl: 'views/register.html',
         controller: 'AuthController'
       })
-      .when('/post', {
-        templateUrl: 'views/post.html',
-        controller: 'TaskController'
-      })
-      .when('/edit/:taskId', {
-        templateUrl: 'views/edit.html',
-        controller: 'TaskController'
-      })
       .when('/browse', {
         templateUrl: 'views/browse.html',
         controller: 'TaskController'
