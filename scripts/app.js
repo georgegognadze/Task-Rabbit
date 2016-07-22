87c9dc9e2399873497b5cf910ba9d558362fce67
George Gognadze <dreamtocode@users.noreply.github.com>
2016-07-23 00:15:23 +0400

    add app.js

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase',
+    'toaster',
+    'angularMoment'
+  ])
+  .constant('FURL', 'https://task-ninjaclone.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/browse.html',
+        controller: 'BrowseController'        
+      })
+      // .when('/changepass', {
+      //   templateUrl: 'views/partials/changepass.html'
+      // })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
